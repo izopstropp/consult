3ff962c77ac4885ee689ba2675c54599843bed5a
Felipe Francisco <felipeizo@hotmail.com>
2020-06-22 15:22:35 -0300

    resolução da incompatibilidade entre navegadores2

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import Vue from "vue";
-// import "babel-polyfill";
+import "babel-polyfill";
 import Antd from "ant-design-vue";
 import App from "./App.vue";
 import "ant-design-vue/dist/antd.css";
